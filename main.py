e45b27cd06e3d1037d877fd0a4a59d2c6250b82e
Vladyslav <vladislavriddl@gmail.com>
2022-09-07 01:22:18 +0300

    Second commit

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 print('import antigravity')
+
 print('import __hello__')
+
 print('from __future__ import braces')
-print('1')
+
+print('*\t\t* *\t\t\t*\t * *\t *\t *  *\n'
+      ' *\t   *  *\t\t   *')
+
+print('''Escape sequences
+\\a\tBell (alert)
+\\b\tBackspace
+\\n\tNew line
+\\t\tHorizontal tab
+\\\tBackslash \\
+\\"\tDouble quotation mark"
+\\'\tSingle quotation mark'
+''')
